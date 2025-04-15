0439fb68d578a3b5a6550aefa6e7820dd99b3904
captainkryga <captainkryga@gmail.com>
2025-04-15 11:58:00 +0200

    add my form data, have fun

--- Assets/Project/Scripts/Service/GoogleFormFeedbackService.cs
+++ Assets/Project/Scripts/Service/GoogleFormFeedbackService.cs
@@ -7,12 +7,12 @@ namespace Project.Scripts.Service
 {
     public class GoogleFormFeedbackService : IFeedbackService
     {
-        private const string FormUrl = "https://docs.google.com/forms/d/e/1FAIpQLSfDg8gdpuFYvmWg4OJ_jPFaxCU_BV8c-g5uFjd9vcln8j2aHQ/formResponse";
+        private const string FormUrl = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSehZvUwuIwbBne24YLoxULlk4L6ctbd9LNt6VS9M5oxcGsmjA/formResponse";
         
         public async UniTask<bool> SubmitFeedback(FeedbackData feedbackData)
         {
             WWWForm form = new WWWForm();
-            form.AddField("entry1", feedbackData.Message);
+            form.AddField("entry.485273778", feedbackData.Message);
 
             using UnityWebRequest www = UnityWebRequest.Post(FormUrl, form);
             await www.SendWebRequest().ToUniTask();
